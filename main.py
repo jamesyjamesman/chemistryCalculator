9571a8ef28fa8ffe4faca3c4f186af3be2607c80
jamesyjamesman <jameson.price@my.uwrf.edu>
2024-09-18 12:30:38 -0500

    Added exception handling. Also, program runs until prompted to exit, in case the user would like to do multiple calculations.

--- main.py
+++ main.py
@@ -34,8 +34,22 @@ def find_molar_mass():
     molar_masses = []
     while not done:
         atomic_number = input("Enter the atomic number of the element you'd like to convert.\n")
-        atomic_mass =  periodictable.elements[int(atomic_number)].mass
-        amount = int(input("Enter the amount to convert.\n"))
+        while True:
+            try:
+                atomic_mass = periodictable.elements[int(atomic_number)].mass
+                break
+            except ValueError:
+                invalid = input("Invalid input. Exit using current information? (y/n)")
+                if invalid == "y":
+                    return molar_masses
+        while True:
+            try:
+                amount = int(input("Enter the amount to convert.\n"))
+                break
+            except ValueError:
+                invalid = input("Invalid input. Use current information? (y/n)")
+                if invalid == "y":
+                    return molar_masses
         molar_mass = atomic_mass * amount
         molar_masses.append(molar_mass)
         if input("done? (y/n)\n").lower() == "y":
@@ -43,20 +57,39 @@ def find_molar_mass():
     return molar_masses
 
 def grams_to_moles():
-    molar_mass = float(input("Enter the molar mass of a compound. (g/mol)"))
-    grams = float(input("How many grams of it are there?"))
+    while True:
+        try:
+            molar_mass = float(input("Enter the molar mass of a compound. (g/mol)"))
+            break
+        except ValueError:
+            print("Invalid input. Try again!")
+    while True:
+        try:
+            grams = float(input("How many grams of it are there?"))
+            break
+        except ValueError:
+            print("Invalid input. Try again!")
     return molar_mass / grams
 
-calculate = int(input("\nWhat would you like to calculate?\n\n"
-                  "Dilution (0)\n"
-                  "Find molar mass (1)\n"
-                  "Convert grams to moles (2)\n"
-                  "Find molar mass, convert grams of it to moles (3)\n"))
-if calculate == 0:
-    print(dilution())
-elif calculate == 1:
-    print(f"That molecule has [{round(sum(find_molar_mass()), 5)} g/mol.]")
-elif calculate == 2:
-    print(f"There is [{round(grams_to_moles(), 5)} mol] in that amount.")
-elif calculate == 3:
-    print(f"{round(float(input("Enter the amount to convert. (g)\n")) / sum(find_molar_mass()), 5)} mol.")
+while True:
+    try:
+        calculate = int(input("\nWhat would you like to calculate?\n\n"
+                      "Exit program (0)\n"
+                      "Dilution (1)\n"
+                      "Find molar mass (2)\n"
+                      "Convert grams to moles (3)\n"
+                      "Find molar mass, convert grams of it to moles (4)\n"))
+        if calculate == 0:
+            exit()
+        elif calculate == 1:
+            print(dilution())
+        elif calculate == 2:
+            print(f"That molecule has [{round(sum(find_molar_mass()), 5)} g/mol.]")
+        elif calculate == 3:
+            print(f"There is [{round(grams_to_moles(), 5)} mol] in that amount.")
+        elif calculate == 4:
+            print(f"{round(float(input("Enter the amount to convert. (g)\n")) / sum(find_molar_mass()), 5)} mol.")
+        else:
+            print("Not an option!")
+    except ValueError:
+        print("Invalid input. Try again!")
